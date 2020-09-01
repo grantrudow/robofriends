988ca99af0a63e6ae61c7c6ed7da105a35766eb9
grantrudow <webdev.grant@gmail.com>
2020-09-01 15:49:28 -0500

    Updates

---
+++ precache-manifest.229f6de23779eaf042fb2810cb115fe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc11665600d395c709ed52c52ddfa591",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4141c2d158b8123062d1",
+    "url": "/robofriends/static/css/main.5abd276f.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4141c2d158b8123062d1",
+    "url": "/robofriends/static/js/main.0bbf0d2f.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
